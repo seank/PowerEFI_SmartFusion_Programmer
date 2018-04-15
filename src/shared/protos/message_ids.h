e0bbfd051832040a9b6e28fe90dee7b62d376a4a
Sean Keys <skeys@powerefi.com>
2018-04-15 00:45:11 -0700

    Add proto defs for ISP
    Provides initial proto defs for integration testing.
    
    NanoPB may need an extensive plugin helper to generate rpc code.
    
    Change-Id: Ibcd66b7c32a4bf3af10281d51824c287b9163418

---
+++ src/shared/protos/message_ids.h
@@ -0,0 +1,15 @@
+/*
+ * message_ids.h
+ *
+ */
+
+#ifndef SHARED_PROTOS_MESSAGE_IDS_H_
+#define SHARED_PROTOS_MESSAGE_IDS_H_
+
+/* Over the wire message IDs */
+typedef enum {
+  ISP_SERVICE = 1,
+  ISP_MESSAGE = 2
+}MessageID;
+
+#endif /* SHARED_PROTOS_MESSAGE_IDS_H_ */
