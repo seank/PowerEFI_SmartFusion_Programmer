be31fe55db2e7eb47806de9fcf71df418ec01b27
Sean Keys <skeys@powerefi.com>
2018-06-03 09:06:35 -0700

    Add State Request/Response to top-level OneOf
    TESTS=bazel run //src/shared/protos:powerefi_protoapi_tests
    
    Change-Id: I7af0c8024d733564820c74603ad32df141d76894

--- Makefile
+++ Makefile
@@ -6,4 +6,6 @@ all: tests
 
 tests:
 	bazel build src/shared/protos:powerefi_protoapi_tests
-	
+
+nanopb_g4m:
+	bash src/shared/protos/build_nano_protos.sh src/shared/protos/nanopb_gen src/shared/protos/isp.proto src/shared/protos/options.proto
--- src/shared/protos/message_ids.h
+++ src/shared/protos/message_ids.h
@@ -6,10 +6,17 @@
 #ifndef SHARED_PROTOS_MESSAGE_IDS_H_
 #define SHARED_PROTOS_MESSAGE_IDS_H_
 
+
 /* Over the wire message IDs */
 typedef enum {
   ISP_SERVICE = 1,
   ISP_MESSAGE = 2
 }MessageID;
+
+typedef struct APIMessage {
+  MessageID id;
+  uint16_t length;
+  uint8_t* wire_buffer;
+}APIMessage_t;
 
 #endif /* SHARED_PROTOS_MESSAGE_IDS_H_ */
